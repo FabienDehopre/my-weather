03f3ba999096a3f8d3d73772bdd0e802f1c1f388
Fabien Dehopré <fabien@dehopre.com>
2023-04-19 21:39:40 +0200

    test the bff user endpoint from angular + login

--- src/MyWeather.Frontend/ClientApp/src/app/app.component.ts
+++ src/MyWeather.Frontend/ClientApp/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {CommonModule, DOCUMENT} from '@angular/common';
+import {Router, RouterOutlet} from '@angular/router';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private readonly http = inject(HttpClient);
+  private readonly document = inject(DOCUMENT);
   title = 'my-weather-frontend';
+  user: unknown;
+
+  ngOnInit(): void {
+    const headers = new HttpHeaders()
+      .set('X-CSRF', '1');
+    this.http.get('/bff/user', { headers }).subscribe({
+      next: (user) => {
+        this.user = user;
+      },
+      error: (error) => {
+        console.log('Error', error);
+        this.document.location.assign('https://localhost:7293/bff/login?redirectUrl=https://localhost:44449/');
+      }
+    });
+  }
 }
