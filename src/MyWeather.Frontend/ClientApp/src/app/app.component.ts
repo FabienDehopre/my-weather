56c8d4faba6b17cd8855fff54716663e1e7297d7
Fabien Dehopré <fabien@dehopre.com>
2023-04-20 08:25:17 +0200

    display the user's claim + logout button

--- src/MyWeather.Frontend/ClientApp/src/app/app.component.ts
+++ src/MyWeather.Frontend/ClientApp/src/app/app.component.ts
@@ -3,6 +3,11 @@ import {CommonModule, DOCUMENT} from '@angular/common';
 import {Router, RouterOutlet} from '@angular/router';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
+interface Claim {
+  type: string;
+  value: string;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -14,12 +19,12 @@ export class AppComponent implements OnInit {
   private readonly http = inject(HttpClient);
   private readonly document = inject(DOCUMENT);
   title = 'my-weather-frontend';
-  user: unknown;
+  user: Claim[] = [];
 
   ngOnInit(): void {
     const headers = new HttpHeaders()
       .set('X-CSRF', '1');
-    this.http.get('/bff/user', { headers }).subscribe({
+    this.http.get<Claim[]>('/bff/user', { headers }).subscribe({
       next: (user) => {
         this.user = user;
       },
@@ -28,5 +33,14 @@ export class AppComponent implements OnInit {
         this.document.location.assign('https://localhost:7293/bff/login?redirectUrl=https://localhost:44449/');
       }
     });
+  }
+
+  logout() {
+    if (this.user) {
+      const logoutUrl = this.user.find(x => x.type === 'bff:logout_url')?.value;
+      if (logoutUrl) {
+        this.document.location.assign(`https://localhost:7293${logoutUrl}`);
+      }
+    }
   }
 }
